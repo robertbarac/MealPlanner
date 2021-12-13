560aa327ce15acfe730c78c13a9390deb3b13d91
Robert Barrios <ryba.9125@gmail.com>
2021-12-13 17:48:05 -0500

    adding the column names of the table and the database

---
+++ foods.py
@@ -0,0 +1,18 @@
+class Food:
+    def __init__(self):
+        pass
+
+    def get_connection(self):
+        pass
+    
+    def add_food(self):
+        pass
+    
+    def modify_food(self):
+        pass
+    
+    def delete_food(self):
+        pass
+    
+    def read_food(self):
+        pass
---
+++ menu.py
@@ -0,0 +1,58 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import *
+import sys
+
+class Menu:
+    def __init__(self):
+        aplicacion = QtWidgets.QApplication([])
+        menu = uic.loadUi("gui/menu.ui")
+        menu.show()
+
+        # Table Configuration
+        menu.table_foods.setHorizontalHeaderLabels(['ID', 'Nombre Alimento', 'Gramos Proteína', 'Gramos Carbohidrato', 'Gramos Grasa'])
+        menu.table_foods.setEditTriggers(QTableWidget.NoEditTriggers)
+        menu.table_foods.setSelectionBehavior(QTableWidget.SelectRows)#comportamiento de selección al hacer clic en un registro de la tabla
+        menu.table_foods.cellClicked.connect(self.select)
+
+        menu.btn_add_food.clicked.connect(self.add)
+        menu.btn_modify_food.clicked.connect(self.modify)
+        menu.btn_delete_food.clicked.connect(self.delete)
+        menu.btn_cancel.clicked.connect(self.cancel)
+        
+        sys.exit(aplicacion.exec())
+    
+    def add(self):
+        print("Hola, soy la acción de agregar")
+
+    def modify(self):
+        print("Hola, soy la acción de modificar")
+
+    def delete(self):
+        print("Hola, soy la acción de eliminar")
+
+    def cancel(self):
+        print("Hola, soy la acción de cancelar")
+
+    def validate_fields(self):
+        pass
+
+    def consult(self):
+        pass
+
+    def select(self):
+        id = self.menu.table_foods.selectedIndexes()[0].data()
+        food_name = self.menu.table_foods.selectedIndexes()[1].data()
+        protein_grams = self.menu.table_foods.selectedIndexes()[2].data()
+        carbos_grams = self.menu.table_foods.selectedIndexes()[2].data()
+        fat_grams = self.menu.table_foods.selectedIndexes()[2].data()
+        print(id, food_name, protein_grams, carbos_grams, fat_grams)
+        self.menu.txtId.setText(id)
+        self.menu.txtNombre.setText(food_name)
+        self.menu.txtCorreo.setText(protein_grams)
+        self.menu.txtNombre.setText(carbos_grams)
+        self.menu.txtCorreo.setText(fat_grams)
+        self.menu.btn_add.setEnabled(False)
+
+    def clean_spaces(self):
+        pass
+
